95936e37d6f74c7bf8fe6edeb9b18a8fb213c8bd
Vincent Puget <puget.vincent@gmail.com>
2021-04-13 11:11:13 +0200

    :lipstick:: lint and format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UnicornListComponent } from './unicorn-list/unicorn-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, UnicornListComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
